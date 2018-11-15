53f85e64d0a9d235e57f87bbaf62617541452fe2
peusterm <manuel.peuster@uni-paderborn.de>
2018-11-15 14:08:46 +0100

    All CLI-based vim-emu integration tests are done.
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- test_integration.sh
+++ test_integration.sh
@@ -25,8 +25,123 @@ if [ $(osm vim-list | grep -c 'test-vim1') == 0 ]; then
 fi
 echo "PASS"
 
+###
+### Test 2
+###
+echo "********* Test 2: on-boarding *********"
+# create VNFDs
+osm vnfd-create vnf/hackfest_1_vnfd.tar.gz
+osm vnfd-create vnf/hackfest_2_vnfd.tar.gz
+# check if VNFDs are there
+if [ $(osm vnfd-list | grep -c 'hackfest1-vnf') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm vnfd-list | grep -c 'hackfest2-vnf') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+# create NSD
+osm nsd-create ns/hackfest_1_nsd.tar.gz
+# check if NSD is there
+if [ $(osm nsd-list | grep -c 'hackfest1-ns') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+echo "PASS"
+
+###
+### Test 3
+###
+echo "********* Test 3: instantiation *********"
+osm ns-create --nsd_name hackfest1-ns --ns_name test-ns1-cli --vim_account test-vim1
+# wait for instantiation (FIXME quick hack)
+echo "Waiting for instantiation ..."
+sleep 20
+echo "done"
+if [ $(osm ns-list | grep -c 'test-ns1-cli') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm ns-list | grep -c 'running') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm ns-list | grep -c 'configured') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm ns-list | grep -c 'done') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
 
 
+###
+### Test 4
+###
+echo "********* Test 4: list commands *********"
+if [ $(osm vim-list | grep -c 'test-vim1') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm vnfd-list | grep -c 'hackfest1-vnf') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm vnfd-list | grep -c 'hackfest2-vnf') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm nsd-list | grep -c 'hackfest1-ns') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+if [ $(osm ns-list | grep -c 'test-ns1-cli') == 0 ]; then
+    echo "FAIL"
+    exit 1
+fi
+echo "PASS"
+
+###
+### OPTIONAL: Manual hold running deployment
+###
+#echo "All test deployments done."
+#read -p "Press any key to continue and terminate... " -n1 -s
+#sleep 2
+
+###
+### Test 5
+###
+echo "********* Test 5: termination *********"
+osm ns-delete test-ns1-cli
+# wait (FIXME quick hack)
+echo "Waiting for termination ..."
+sleep 10
+echo "done"
+if [ $(osm ns-list | wc -l) -ne 4 ]; then
+    echo "FAIL"
+    exit 1
+fi
+
+###
+### Test 6
+###
+echo "********* Test 6: delete descriptors *********"
+# delete NSD
+osm nsd-delete hackfest1-ns
+if [ $(osm nsd-list | wc -l) -ne 4 ]; then
+    echo "FAIL"
+    exit 1
+fi
+# delete VNFDs
+osm vnfd-delete hackfest1-vnf
+osm vnfd-delete hackfest2-vnf
+if [ $(osm vnfd-list | wc -l) -ne 4 ]; then
+    echo "FAIL"
+    exit 1
+fi
+echo "PASS"
 
 ###
 ### Test 7
