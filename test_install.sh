70c5008aec7b557b15f6af48857048bd910f1f93
peusterm <manuel.peuster@uni-paderborn.de>
2019-11-25 16:15:27 +0100

    Fix
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- test_install.sh
+++ test_install.sh
@@ -20,3 +20,6 @@ docker rm -f vim-emu || true
 
 # install new OSM with vim-emu
 ./install_osm.sh --vimemu 2>&1 | tee logs/osm_install.log
+
+# add ELK stack
+./install_osm.sh -o elk_stack | tee logs/osm_install_elk.log
