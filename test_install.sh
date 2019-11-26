a8e3c4e744f1a47d4a64dfb18c13994e8dc738cf
peusterm <manuel.peuster@uni-paderborn.de>
2019-11-26 14:42:02 +0100

    Prepared installer script for Rel. SEVEN
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- test_install.sh
+++ test_install.sh
@@ -9,7 +9,7 @@ pwd
 rm -f install_osm.sh
 
 # get latest installer
-wget https://osm-download.etsi.org/ftp/osm-6.0-six/install_osm.sh
+wget https://osm-download.etsi.org/ftp/osm-7.0-seven/install_osm.sh
 chmod +x install_osm.sh
 
 # uninstall old OSM
@@ -19,7 +19,7 @@ docker stop vim-emu || true
 docker rm -f vim-emu || true
 
 # install new OSM with vim-emu
-./install_osm.sh --vimemu 2>&1 | tee logs/osm_install.log
+./install_osm.sh -y -r testing -R ReleaseSIX-daily -u https://osm-download.etsi.org/repository/osm/debian -t releasesix-daily --vimemu 2>&1 | tee logs/osm_install.log 
 
 # add ELK stack
-./install_osm.sh -o elk_stack | tee logs/osm_install_elk.log
+#./install_osm.sh -o elk_stack | tee logs/osm_install_elk.log
